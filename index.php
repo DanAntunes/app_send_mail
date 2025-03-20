1e3020e6866d8395bb133defac32d591d0d3ed3e
Soren <danilo-258@hotmail.com>
2025-03-20 16:14:42 -0300

    feat(index): reorganizar links de CSS
    - Altera a ordem dos links de CSS para carregar primeiro o Bootstrap e depois a folha de estilo principal, garantindo uma melhor cascata de estilos.
    - Mantém os metadados essenciais, SEO e configurações PWA intactos.

--- index.php
+++ index.php
@@ -24,8 +24,12 @@
   <link rel="icon" type="image/png" sizes="32x32" href="./src/assets/images/favicon/favicon-32x32.png">
   <link rel="icon" type="image/png" sizes="16x16" href="./src/assets/images/favicon/favicon-16x16.png">
 
-    <link rel="stylesheet" href="./src/assets/css/style.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <!-- Framework CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+  <!-- Folha de estilo principal -->
+  <link rel="stylesheet" href="./src/assets/css/style.css">
+   
 </head>
 <body class="bg-light">
  <header class="py-5 text-center">
