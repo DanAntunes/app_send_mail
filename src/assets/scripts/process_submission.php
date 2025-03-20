a22c88311a1d8d4f33e34d7d2acd0007ede5214f
Soren <danilo-258@hotmail.com>
2025-03-20 01:31:33 -0300

    feat(process_submission): integrar envio de e-mail via PHPMailer
    - Altera verificação da mensagem para encerrar a execução caso a validação falhe.
    - Adiciona configuração do PHPMailer para envio de e-mails via SMTP (host, autenticação, porta, etc.).
    - Implementa bloco try/catch para tratamento de erros durante o envio.

--- src/assets/scripts/process_submission.php
+++ src/assets/scripts/process_submission.php
@@ -37,13 +37,46 @@ $message-> __set('subject', $_POST['subject']);
 $message-> __set('message', $_POST['message']);
 
 
-if ($message->validMessage()){
-  echo 'Mensagem Valida';
-} else {
-  echo 'Mensagem não é valida';
+if(!$mensagem->mensagemValida()) {
+  echo 'Mensagem não é válida';
+  die();
 }
 
+$mail = new PHPMailer(true);
+try {
+    //Server settings
+    $mail->SMTPDebug = 2;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = '';                     //SMTP username
+    $mail->Password   = '';                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+    $mail->Port       = 587;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
+    //Recipients
+    $mail->setFrom('', '');
+    $mail->addAddress('', '');     //Add a recipient
+    //$mail->addAddress('ellen@example.com');               //Name is optional
+    //$mail->addReplyTo('info@example.com', 'Information');
+    //$mail->addCC('cc@example.com');
+    //$mail->addBCC('bcc@example.com');
 
+    //Attachments
+    //$mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+    //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'Oi, como vai ?';
+    $mail->Body    = 'Olá, como vai <b>Soren!</b>';
+    $mail->AltBody = 'Olá, como vai Soren!';
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Não foi possivel enviar este e-mail! Por favor tente novamente mais tarde.";
+    echo 'Detalhes do erro: ' . $mail->ErrorInfo;
+}
 
 ?>
